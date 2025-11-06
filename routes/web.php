dd7bc9aad0fc19ceacd79c40ec12e8208a54a61d
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-05 22:03:30 -0300

    nova: criação de todas as rotas da aplicação

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskTrashController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('login.index');
+Route::middleware('guest')->group(function () {
+
+Route::get('/', [LoginController::class, 'login'])->name('login.index');
+
+Route::post('/auth', [LoginController::class, 'authUser'])->name('login.auth');
+
+Route::get('/register', [RegisterController::class, 'register'])->name('register.index');
+
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
 });
 
-Route::resource('tasks', TaskController::class);
+Route::middleware('auth')->group(function () {
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::resource('tasks', TaskController::class);
+
+    Route::get('lixeira', [TaskTrashController::class, 'index'])->name('tasks.trash');
+
+    Route::put('lixeira/{task}/restore', [TaskTrashController::class, 'update'])->name('tasks.restore');
+
+    Route::delete('lixeira/{task}/force-delete', [TaskTrashController::class, 'destroy'])->name('tasks.forceDelete');
+});
