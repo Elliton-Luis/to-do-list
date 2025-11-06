cf22b27e6d3cb853d1f3ba4ed814e5d12a9a82fe
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-05 22:06:14 -0300

    config: Estilização da paginação da aplicação

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,9 +1,8 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="Navegação" class="flex items-center justify-between mt-8">
-        {{-- Visualização Mobile --}}
         <div class="flex justify-between flex-1 sm:hidden">
             @if ($paginator->onFirstPage())
-                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-400 bg-gray-50 border border-gray-300 cursor-default leading-5 rounded-md">
+                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-400 bg-white border border-gray-300 cursor-default leading-5 rounded-md opacity-60">
                     {!! __('pagination.previous') !!}
                 </span>
             @else
@@ -17,13 +16,12 @@
                     {!! __('pagination.next') !!}
                 </a>
             @else
-                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-400 bg-gray-50 border border-gray-300 cursor-default leading-5 rounded-md">
+                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-400 bg-white border border-gray-300 cursor-default leading-5 rounded-md opacity-60">
                     {!! __('pagination.next') !!}
                 </span>
             @endif
         </div>
 
-        {{-- Visualização Desktop --}}
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
             <div>
                 <p class="text-sm text-gray-700 leading-5">
@@ -43,7 +41,6 @@
 
             <div>
                 <span class="relative z-0 inline-flex rtl:flex-row-reverse shadow-sm rounded-md">
-                    {{-- Botão "Anterior" Desktop --}}
                     @if ($paginator->onFirstPage())
                         <span aria-disabled="true" aria-label="Anterior">
                             <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-400 bg-gray-50 border border-gray-300 cursor-default rounded-l-md leading-5" aria-hidden="true">
@@ -60,24 +57,19 @@
                         </a>
                     @endif
 
-                    {{-- Links das Páginas --}}
                     @foreach ($elements as $element)
-                        {{-- "Três Pontos" (...) --}}
                         @if (is_string($element))
                             <span aria-disabled="true">
                                 <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5">{{ $element }}</span>
                             </span>
                         @endif
 
-                        {{-- Array de Links (Números) --}}
                         @if (is_array($element))
                             @foreach ($element as $page => $url)
-                                {{-- Página Atual (Roxo) --}}
                                 @if ($page == $paginator->currentPage())
                                     <span aria-current="page">
                                         <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-indigo-600 border border-indigo-600 cursor-default leading-5">{{ $page }}</span>
                                     </span>
-                                {{-- Outras Páginas (Branco) --}}
                                 @else
                                     <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring ring-indigo-300 focus:border-indigo-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="Ir para a página {{ $page }}">
                                         {{ $page }}
@@ -87,7 +79,6 @@
                         @endif
                     @endforeach
 
-                    {{-- Botão "Próxima" Desktop --}}
                     @if ($paginator->hasMorePages())
                         <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-600 bg-white border border-gray-300 rounded-r-md leading-5 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring ring-indigo-300 focus:border-indigo-300 active:bg-gray-100 active:text-gray-600 transition ease-in-out duration-150" aria-label="Próxima">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
