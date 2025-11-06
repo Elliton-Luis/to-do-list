3a6ddb4eaaebe3e13cabdb672ebb8856ffe04788
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-05 22:06:56 -0300

    estilo: ajuste no estilo do layout padrão

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title', 'To-Do List')</title>
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @livewireStyles
 </head>
 
 <body class="bg-stone-100 text-stone-900 antialiased"
@@ -20,15 +21,16 @@
          class="fixed inset-0 bg-black/50 z-30 md:hidden" style="display: none;">
     </div>
 
-
-    <main class="md:pl-64">
+    <main class="md:pl-64 min-h-screen">
         <div class="pt-16 md:pt-10">
             <div class="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-2xl border border-stone-200 sm:p-8">
                 @include('partials.messages')
                 @yield('content')
             </div>
         </div>
+        <div class="h-10"></div>
     </main>
 
+    @livewireScripts
 </body>
 </html>
